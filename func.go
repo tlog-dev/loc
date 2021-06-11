935b77546abdef045d9f5fffe5addc5b58e8ba20
nikandfor <nikandfor@gmail.com>
2021-06-11 11:00:46 +0300

    FuncentryFromFunc

---
+++ func.go
@@ -0,0 +1,57 @@
+package loc
+
+import (
+	"reflect"
+	"unsafe"
+)
+
+type (
+	// rtype is the common implementation of most values.
+	// It is embedded in other struct types.
+	//
+	// rtype must be kept in sync with ../runtime/type.go:/^type._type.
+	rtype struct {
+		size       uintptr
+		ptrdata    uintptr // number of bytes in the type that can contain pointers
+		hash       uint32  // hash of type; avoids computation in hash tables
+		tflag      uint8   // extra type information flags
+		align      uint8   // alignment of variable with this type
+		fieldAlign uint8   // alignment of struct field with this type
+		kind       uint8   // enumeration for C
+		// function for comparing objects of this type
+		// (ptr to object A, ptr to object B) -> ==?
+		equal  func(unsafe.Pointer, unsafe.Pointer) bool
+		gcdata *byte // garbage collection data
+		//	str       nameOff // string form
+		//	ptrToThis typeOff // type for pointer to this type, may be zero
+	}
+
+	fface struct {
+		t *rtype
+		e *uintptr
+	}
+)
+
+const (
+	kindDirectIface = 1 << 5
+	kindGCProg      = 1 << 6 // Type.gc points to GC program
+	kindMask        = (1 << 5) - 1
+)
+
+func FuncentryFromFunc(f interface{}) PC {
+	ff := (*fface)(unsafe.Pointer(&f))
+
+	if ff.t == nil {
+		return 0
+	}
+
+	if reflect.Kind(ff.t.kind&kindMask) != reflect.Func {
+		panic("not a function")
+	}
+
+	if ff.e == nil {
+		return 0
+	}
+
+	return PC(*ff.e)
+}
---
+++ func_test.go
@@ -0,0 +1,68 @@
+package loc
+
+import (
+	"reflect"
+	"testing"
+	"unsafe"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type eface struct {
+	t unsafe.Pointer
+	d unsafe.Pointer
+}
+
+func TestFuncFunc(t *testing.T) {
+	var f interface{}
+
+	f = TestSetCache
+	r := reflect.ValueOf(f)
+	t.Logf("reflect: %v %v %v %x", r.Kind(), r.Type(), r, *(*eface)(unsafe.Pointer(&f)))
+
+	f = TestFuncFunc
+	r = reflect.ValueOf(f)
+	t.Logf("reflect: %v %v %v %x", r.Kind(), r.Type(), r, *(*eface)(unsafe.Pointer(&f)))
+
+	pc := FuncentryFromFunc(TestFuncFunc)
+
+	name, file, line := pc.NameFileLine()
+	t.Logf("pc: %v %v %v", name, file, line)
+
+	assert.Equal(t, Funcentry(0), pc)
+
+	assert.Equal(t, Funcentry(0), PC(reflect.ValueOf(TestFuncFunc).Pointer()))
+
+	var q func()
+
+	name, file, line = FuncentryFromFunc(q).NameFileLine()
+	t.Logf("pc: %v %v %v", name, file, line)
+
+	var e PC
+	q = func() {
+		t.Logf("closure func")
+
+		e = Funcentry(0)
+	}
+
+	q()
+
+	rt := reflect.ValueOf(q).Type()
+	for i := 0; i < rt.NumIn(); i++ {
+		t.Logf("q in  %v", rt.In(i))
+	}
+	for i := 0; i < rt.NumOut(); i++ {
+		t.Logf("q out %v", rt.Out(i))
+	}
+
+	pc = FuncentryFromFunc(q)
+
+	name, file, line = pc.NameFileLine()
+	t.Logf("pc: %v %v %v", name, file, line)
+
+	assert.Equal(t, e, pc)
+
+	assert.Panics(t, func() {
+		pc = FuncentryFromFunc(3)
+	})
+}
